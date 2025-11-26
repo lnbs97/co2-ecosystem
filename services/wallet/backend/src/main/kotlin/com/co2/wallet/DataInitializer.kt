6357f961ee2633315974ac886994232fa5e5b25d
Leo Nobis <45437638+lnbs97@users.noreply.github.com>
2025-11-26 13:51:14 +0100

    refactor initial wallet creation

--- services/wallet/backend/src/main/kotlin/com/co2/wallet/DataInitializer.kt
+++ services/wallet/backend/src/main/kotlin/com/co2/wallet/DataInitializer.kt
@@ -8,30 +8,18 @@ import org.springframework.stereotype.Component
 class DataInitializer(private val walletService: WalletService) : CommandLineRunner {
 
     override fun run(vararg args: String?) {
-        try {
-            walletService.createWallet(CreateWalletRequest("person_a_uuid", 100.0, 500.0))
-        } catch (e: Exception) {
-            println("Wallet for person_a_uuid already exists.")
-        }
-        try {
-            walletService.createWallet(CreateWalletRequest("person_b_uuid", 100.0, 500.0))
-        } catch (e: Exception) {
-            println("Wallet for person_b_uuid already exists.")
-        }
-        try {
-            walletService.createWallet(CreateWalletRequest("shop_a_uuid", 10000.0, 100000.0))
-        } catch (e: Exception) {
-            println("Wallet for shop_a_uuid already exists.")
-        }
-        try {
-            walletService.createWallet(CreateWalletRequest("shop_b_uuid", 10000.0, 100000.0))
-        } catch (e: Exception) {
-            println("Wallet for shop_b_uuid already exists.")
-        }
-        try {
-            walletService.createWallet(CreateWalletRequest("exchange", 0.0, 0.0))
-        } catch (e: Exception) {
-            println("Wallet for exchange already exists.")
+        listOf(
+            CreateWalletRequest("person_a_uuid", 100.0, 500.0),
+            CreateWalletRequest("person_b_uuid", 100.0, 500.0),
+            CreateWalletRequest("shop_a_uuid", 10000.0, 100000.0),
+            CreateWalletRequest("shop_b_uuid", 10000.0, 100000.0),
+            CreateWalletRequest("exchange", 0.0, 0.0)
+        ).forEach { request ->
+            try {
+                walletService.createWallet(request)
+            } catch (e: Exception) {
+                println("Wallet for ${request.userId} already exists.")
+            }
         }
     }
 }
